12aeb712e27fc694d7f23113f1683a0ca05145c6
github username <juliannne@gmail.com>
2018-06-02 16:17:11 -0400

    Add event listeners to setup and add reveal method

--- memoryGame.js
+++ memoryGame.js
@@ -1,21 +1,49 @@
+var clickedArray = [];
+
 setUp();
 
 function randomNumbers(){
-    var numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5];
-    numbers.sort(function(){
-        return 0.5 - Math.random();
-    })
-    return numbers;
+  var numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5];
+  numbers.sort(function(){
+    return 0.5 - Math.random();
+  })
+  return numbers;
+}
+
+function reveal(cell){
+  cell.style.backgroundColor = " #ffff66";
+  cell.innerHTML = cell.value;
+  cell.clicked = true;
 }
 
 function setUp(){
-    var grid = document.getElementsByTagName("td");
-    var randomized = randomNumbers();
+  var grid = document.getElementsByTagName("td");
+  var randomized = randomNumbers();
 
-    for(var i = 0; i < grid.length; i++){
-        var cell = grid[i];
-        cell.completed = false;
-        cell.clicked = false;
-        cell.value = randomized[i];
-    }
+  for(var i = 0; i < grid.length; i++){
+    var cell = grid[i];
+    // becomes true if "completed" bc it matched with its matching pair
+    cell.completed = false;
+    // tells whether or not a cell is currently clicked
+    cell.clicked = false;
+    // represents hidden value of the cell
+    cell.value = randomized[i];
+
+    cell.addEventListener("mouseenter", function(){
+      if(this.completed == false && this.clicked == false)
+        this.style.background = "#ff3385";
+    });
+
+    cell.addEventListener("mouseleave", function(){
+      if(this.completed == false && this.clicked == false)
+        this.style.background = "#66ffcc";
+    });
+
+    cell.addEventListener('click',function(){
+      if(this.clicked == false && this.completed == false){
+        clickedArray.push(this);
+        reveal(this);
+      }
+    });
+  }
 }
